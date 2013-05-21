c7a4060e5e7a70ec13803a5d4c2e972400fb7f99
janeeats <jane.vora@gmail.com>
2013-05-20 20:01:27 -0400

    add folder model with association to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
-  attr_accessible :email, :password, :password_confirmation, :username
+  attr_accessible :email, :password, :password_confirmation, :username, :folders
   validates_uniqueness_of :email, :username
+
+  has_many :folders
 end
