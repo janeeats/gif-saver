24b35e0179107ca4f4b2b8a95ada5ab192e2f219
janeeats <jane.vora@gmail.com>
2013-05-23 19:52:08 -0400

    add ability to upload gifs via url

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -1,7 +1,23 @@
+require 'open-uri'
+
 class Gif < ActiveRecord::Base
-  attr_accessible :file_path, :folder_id, :file
+
+  attr_accessible :file, :folder_id, :file_remote_url
+
+  has_attached_file :file, :styles => { :medium => "375x300>", :thumb => "100x80>" }, :default_url => "/assets/default.gif"
 
   belongs_to :folder
 
-  has_attached_file :file, :styles => { :medium => "375x300>", :thumb => "100x80>" }, :default_url => "/assets/default.gif"
+  def file_url_provided?
+    !self.file_remote_url.blank?
+  end
+  
+  def download_remote_gif(file_remote_url)
+    if file_url_provided?
+      self.file = open(URI.parse(file_remote_url)) 
+    else
+      # => do nothing
+    end
+  end
+
 end
