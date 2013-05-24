2c54d767c819e645ee495f0c60ae8ad0807cf722
janeeats <jane.vora@gmail.com>
2013-05-23 20:32:17 -0400

    clean up file names of uploads via url

--- app/models/gif.rb
+++ app/models/gif.rb
@@ -15,9 +15,14 @@ class Gif < ActiveRecord::Base
   def download_remote_gif(file_remote_url)
     if file_url_provided?
       self.file = open(URI.parse(file_remote_url)) 
+      self.file_file_name = parse_url_for_name
     else
       # => do nothing
     end
+  end
+
+  def parse_url_for_name
+    self.file_remote_url.split("/").last
   end
 
 end
