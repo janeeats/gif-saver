766b5dc99f473a34500f5a2b765b1ee8ee35f350
janeeats <jane.vora@gmail.com>
2013-05-30 23:08:20 -0400

    more fixes to url path changes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
     user = User.find_by_email(params[:email])
     if user && user.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
+      redirect_to user_gifs_path(@current_user), :notice => "Logged in!"
     else
       render "new", :notice => "Email or password is invalid"
     end
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root_url, :notice => "Logged out!"
+    redirect_to log_in_path, :notice => "Logged out!"
   end
 
   def new_facebook
@@ -32,7 +32,7 @@ class SessionsController < ApplicationController
 
     # Log the authorizing user in.
     current_user = @auth.user
-    redirect_to root_url, :notice => "Thanks for logging in with Facebook, #{current_user.username}."
+    redirect_to user_gifs_path(@current_user), :notice => "Thanks for logging in with Facebook, #{current_user.username}."
   end
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       session[:user_id] = @user.id
-      redirect_to root_url, notice: "Thank you for signing up!"
+      redirect_to log_in_path, notice: "Thank you for signing up!"
     else
       render "new"
     end
