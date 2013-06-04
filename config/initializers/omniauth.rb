6e178b6d06a442d0f9b817451e996fcd46e332f0
janeeats <jane.vora@gmail.com>
2013-06-03 20:47:58 -0400

    typo in config file

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,4 +4,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY_DEV'], ENV['FACEBOOK_SECRET_DEV'],
            { :scope => 'publish_stream,publish_actions' }
   provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET'] 
-end.
+end
